4932689453d77feb94822527dd9416df9465e708
Paul Seydel <seydel@zib.de>
2020-11-16 15:54:25 +0100

    fixed ONE issue in handlers.rs, added swp-files to gitignore

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,17 +1,17 @@
 pub mod geoloc {
-    use actix_web::{ http::StatusCode, HttpRequest, HttpResponse, Responder};
+        use actix_web::{ http::StatusCode, HttpRequest, HttpResponse, Responder, dev};
 
     pub async fn add_location(req: HttpRequest) -> impl Responder {
         println!("addLocation called!");
-        HttpResponse::Ok();
+        HttpResponse::Ok()
     }
 
     pub async fn get_locations(req: HttpRequest) -> impl Responder {
         println!("getLocations called!");
-        HttpResponse::Ok();
+        HttpResponse::Ok()
     }
 
-    pub async fn return_not_found(req: HttpRequest) -> impl Responder {
-        HttpResponse::new(StatusCode::NOT_FOUND);
+    pub async fn return_not_found(r: dev::ServiceResponse) -> impl Responder {
+        HttpResponse::new(StatusCode::NOT_FOUND)
     }
 }
